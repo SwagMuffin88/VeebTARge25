dfe8e9c37ae6be5fc3fe5f4755f45148a831e213
Katri Lotamõis <katrilotamois@outlook.com>
2026-04-29 14:25:15 +0300

    add api call lecture code

--- 05-api-calls/chuck-norris-jokes/src/App.tsx
+++ 05-api-calls/chuck-norris-jokes/src/App.tsx
@@ -1,121 +1,96 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { motion, AnimatePresence } from "framer-motion";
 import './App.css'
 
+interface JokesResponse {
+  value: string
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [joke, setJoke] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const fetchJoke = async () => {
+    setLoading(true)
+    setError(null)
+
+    try {
+      const response = await fetch('https://api.chucknorris.io/jokes/random')
+
+      if(!response.ok) {
+        throw new Error(response.statusText)
+      }
+
+      const data: JokesResponse = await response.json()
+      setJoke(data.value)
+    } catch (error) {
+      setError("Failed to fetch joke: " + error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+      <div className="min-h-screen flex flex-col itmes-center justify-center bg-gray-100 p-4">
+        <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.4 }}
+            className="bg-white shadow-lg rounded-2x1 p-6 max-w-md text-center"
         >
-          Count is {count}
-        </button>
-      </section>
+          <h1 className="text-2x1 font-bold mb-4">Chuck Norris Joke</h1>
 
-      <div className="ticks"></div>
+          <div className="min-h-[80px] flex items-center justify-center">
+            <AnimatePresence mode="wait">
+              {loading && (
+                  <motion.p
+                      key="loading"
+                      initial={{ opacity: 0, y: 10 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: -10 }}
+                  >
+                    Loading...
+                  </motion.p>
+              )}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+              {error && (
+                  <motion.p
+                      key="error"
+                      className="text-red-500"
+                      initial={{ opacity: 0, y: 10 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: -10 }}
+                  >
+                    {error}
+                  </motion.p>
+              )}
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+              {!loading && !error && joke && (
+                  <motion.p
+                      key={joke}
+                      className="mb-4"
+                      initial={{ opacity: 0, y: 20 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: -20 }}
+                      transition={{ duration: 0.3}}
+                  >
+                    {joke}
+                  </motion.p>
+              )}
+            </AnimatePresence>
+          </div>
+
+          <motion.button
+              onClick={fetchJoke}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className="bg-blue-500 text-white px-4 py-2 rounded-x1 hover:bg-blue-600 transition"
+          >
+            Get New Joke
+          </motion.button>
+        </motion.div>
+      </div>
   )
 }
 
